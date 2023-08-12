0f6ddec1f2eb9476072229cbdcdbaf564329c42e
José S. Reyes <37203999+josesreyesdev@users.noreply.github.com>
2023-08-11 18:09:24 -0600

    Update CreateConnection.java

--- src/main/java/com/jsrdev/CreateConnection.java
+++ src/main/java/com/jsrdev/CreateConnection.java
@@ -1,17 +1,1 @@
-package com.jsrdev;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class CreateConnection {
-
-    // recuperar conexion
-    public Connection retrieveConnection() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_stock?useTimeZone=true&serverTimeZone=UTC",
-                "root",
-                "Sarj950905"
-        );
-    }
-}
